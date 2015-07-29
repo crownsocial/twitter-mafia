362a8091374f86ad473a6c4651374bc2487352af
Timon <timonpike@gmail.com>
2015-07-29 10:26:53 -0700

    Split search terms based on average character length and make multiple async calls.

--- config/cron.js
+++ config/cron.js
@@ -54,6 +54,44 @@ var checkDataType = function(tracker, tweets) {
   return obj;
 }
 
+var collectTwitterData = function(terms, callback) {
+  console.log('Searching twitter for:',terms.q);
+
+  searchParams = {
+    q: terms.q,
+    result_type: 'recent',
+    count: 100
+  }
+
+  if(terms.latest_id !== "0") {
+    searchParams.since_id = terms.latest_id;
+  } else {
+    searchParams.q += ' since:' + utility.getPreviousDate();
+  }
+  // console.log('search params:',searchParams);
+
+  client.get('search/tweets', searchParams, function(err, data, response){
+    console.log("error?",err)
+
+    if (!err){
+      if(data.statuses.length === 0) {
+        callback(null, {});
+        return;
+      }
+
+      var trackerData = {};
+      for (var i = 0; i < account.trackers.length; i++){
+        trackerData[account.trackers[i].name+'.'+account.trackers[i].type] = checkDataType(account.trackers[i], data.statuses);
+      }
+      callback(null, trackerData);
+    } else {
+      callback(err);
+    }
+  });
+}
+
+const MAX_CALL_LENGTH = 30;
+
 module.exports.cron = {
   '*/30 * * * * *': function() {
     Twitter_Account.find().populateAll().exec(function(err, data){
@@ -75,67 +113,65 @@ module.exports.cron = {
               case 'mention':
                 return '@' + tracker.name + ' -RT';
             }
-          }).join(' OR ');
-          console.log('Searching twitter for:',qTerms);
-
-          searchParams = {
-            q: qTerms,
-            result_type: 'recent',
-            count: 100
-          }
-
-          if(latest_id !== "0") {
-            searchParams.since_id = latest_id;
-          } else {
-            searchParams.q += ' since:' + utility.getPreviousDate();
-          }
-          // console.log('search params:',searchParams);
-
-          client.get('search/tweets', searchParams, function(err, data, response){
-            console.log("error?",err)
-            if (!err){
-              var trackerData = {};
-              for (var i = 0; i < account.trackers.length; i++){
-                trackerData[account.trackers[i].name+'.'+account.trackers[i].type] = checkDataType(account.trackers[i], data.statuses);
-                // if (Object.keys(obj).length == account.trackers.length){
-                //   break;
-                // }
-              }
-              if(false) {
-                fs.writeFile("../tweetData.json", JSON.stringify(data), function(err) {
-                  if(err) {
-                    return console.log(err);
-                  }
-                  console.log("The tweet file was saved!");
-                });
-                fs.writeFile("../trackerData.json", JSON.stringify(trackerData), function(err) {
-                  if(err) {
-                    return console.log(err);
-                  }
-                  console.log("The obj file was saved!");
-                });
-              }
-              console.log('num of tweets:',data.statuses.length);
-              console.log('\n**************************************************\n');
-              // console.log('trackerData:', trackerData)
-              // console.log('\n**************************************************\n');
-
-              if(data.statuses.length !== 0) {
-                utility.sendEmail(account.user.email, "[TwitterMafiaApp] You have new updates!", trackerData, false);
-                async.each(Object.keys(trackerData), function(key, callback){
-                  var keyArr = key.split('.');
-                  if(trackerData[key].latest_id > 0) {
-                    console.log('running async...',key)
-                    Tracker.update({name: keyArr[0], type: keyArr[1]}, {data: trackerData[key]}).exec(function(err, updated){
-                      console.log(updated)
-                      callback();
-                    }, function(err){
-                    })
-                  }
-                })
-              }
-            }
           })
+          var callTimes = Math.ceil(qTerms.length / MAX_CALL_LENGTH)
+          var qSplits = callTimes > 1 ? [] : [{q: qTerms.join(' OR '), latest_id: latest_id}];
+          for(var i = 0; i < callTimes; i++) {
+            var start = (i)*MAX_CALL_LENGTH
+            var end = (i+1)*MAX_CALL_LENGTH
+            if(end > qTerms.length) end = qTerms.length - 1
+            qSplits.push({q: qTerms.slice(start, end).join(' OR '), latest_id: latest_id})
+          }
+
+          async.mapSeries(qSplits, collectTwitterData, function(err, results) {
+            if(err) console.log("Async Twitter Call ERROR:",err);
+
+            var trackerData = results.reduce(function(obj, curr) {
+              for(var key in curr) {
+                if(obj[key]) {
+                  obj[key].latest_id = curr[key].latest_id > obj[key].latest_id ? curr[key].latest_id : obj[key].latest_d;
+                  obj[key].data.concat(curr[key].data);
+                } else {
+                  obj[key] = curr[key];
+                }
+              }
+              return obj;
+            });
+
+            var numOfTweets = Object.keys(trackerData).length;
+
+            if(false) {
+              fs.writeFile("../tweetData.json", JSON.stringify(data), function(err) {
+                if(err) {
+                  return console.log(err);
+                }
+                console.log("The tweet file was saved!");
+              });
+              fs.writeFile("../trackerData.json", JSON.stringify(trackerData), function(err) {
+                if(err) {
+                  return console.log(err);
+                }
+                console.log("The obj file was saved!");
+              });
+            }
+            console.log('num of tweets:',data.statuses.length);
+            console.log('\n**************************************************\n');
+
+            if(numOfTweets > 0) {
+              utility.sendEmail(account.user.email, "[TwitterMafiaApp] You have new updates!", trackerData, false);
+              async.each(Object.keys(trackerData), function(key, callback){
+                var keyArr = key.split('.');
+                if(trackerData[key].latest_id > 0) {
+                  console.log('running async...',key)
+                  Tracker.update({name: keyArr[0], type: keyArr[1]}, {data: trackerData[key]}).exec(function(err, updated){
+                    console.log(updated)
+                    callback();
+                  }, function(err){
+                  });
+                }
+              });
+            }
+          });
         } else {
           console.log(account.user.username, 'update skipped');
         }
